11d074bff474eac3de79ed372b5119ca94034947
robs <mnnnaleudes@gmail.com>
2022-01-03 10:46:17 -0300

    add support route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import RoomController from "@controllers/RoomController";
+import SupportController from "@controllers/SupportController";
 
 const routes = Router();
 
 const Room = new RoomController;
+
+const Support = new SupportController;
 
 /*
 room routes
@@ -23,6 +26,23 @@ routes.post('/room', Room.store);
 routes.put('/room', Room.update);
 
 routes.get('/room', Room.list);
+
+/*
+end room routes
+ */
+
+/*
+support routes
+
+name:
+email:
+status:
+
+ */
+
+routes.put('/support', Support.update);
+
+routes.get('/support', Support.list);
 
 /*
 end room routes
